35aaa7ef6bc7586d7c375240fdcfddeb2a2d584d
jiangdan <466375295@qq.com>
2016-08-11 22:09:27 +0800

    客户单号管理

---
+++ src/main/java/com/oa/model/Agreement.java
@@ -0,0 +1,91 @@
+package com.oa.model;
+
+import java.util.Date;
+
+/**
+ * 报价单号表
+ * Created by 46637 on 2016/8/11.
+ */
+public class Agreement {
+    private Long id;
+    private String priceNum;//报价单号
+    private Double salary;//薪资
+    private Long customId;//客户id
+    private Integer isDel;//删除状态 0 未删除 1 已删除
+    private Long creater;//创建人
+    private Date createDate;//创建时间
+    private Long updater;//修改人
+    private Date updateDate;//修改时间
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPriceNum() {
+        return priceNum;
+    }
+
+    public void setPriceNum(String priceNum) {
+        this.priceNum = priceNum;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Long getCustomId() {
+        return customId;
+    }
+
+    public void setCustomId(Long customId) {
+        this.customId = customId;
+    }
+
+    public Integer getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Integer isDel) {
+        this.isDel = isDel;
+    }
+
+    public Long getCreater() {
+        return creater;
+    }
+
+    public void setCreater(Long creater) {
+        this.creater = creater;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Long getUpdater() {
+        return updater;
+    }
+
+    public void setUpdater(Long updater) {
+        this.updater = updater;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+}
